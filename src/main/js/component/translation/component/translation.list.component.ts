a384960b2d5954591cc132d54268c7ebfecbd7da
Zainabed <zainabed@gmail.com>
2018-05-15 12:10:31 +0530

    Added key search functionality.

--- src/main/js/component/translation/component/translation.list.component.ts
+++ src/main/js/component/translation/component/translation.list.component.ts
@@ -4,6 +4,7 @@ import { ResourceListComponent, ResourcePath } from "../../../lib/component/reso
 import { ProjectService, ProjectResourceListComponent } from "../../project/project.core";
 import { Translation } from "../model/translation";
 import { KeyService } from "../../key/model/key.service";
+import { KeyForm } from "../../key/form/key.form";
 
 @ResourcePath({
     path: "keys",
@@ -19,10 +20,25 @@ import { KeyService } from "../../key/model/key.service";
 })
 export class TranslationListComponent extends ProjectResourceListComponent<Translation> {
     keyService: KeyService;
+    keyForm: KeyForm;
+    searchUrl : string;
 
     constructor(injector: Injector) {
         super(injector);
         this.keyService = injector.get(KeyService);
+        this.keyForm = injector.get(KeyForm);
+    }
+
+    ngOnInit(){
+        super.ngOnInit();
+        this.searchUrl = this.appData.getResourceListUrlFor(this.appData.resource, this._path);
+        this.searchUrl += "/search/name?projects=" + this.projectId + "&name="; 
+    }
+
+    search(){
+        let key = this.keyForm.getData();
+        this.apiUrl = this.searchUrl +  key.name;
+        this.get();
     }
 
     open(resource) {
